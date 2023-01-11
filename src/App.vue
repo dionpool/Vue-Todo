60e135c05be9e2ccd0f7acd088c4e1d3bce6cb45
Margrietj <margrietjellema32@gmail.com>
2023-01-11 14:45:40 +0100

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import todoItem from './components/todoItem.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Todo list</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <div class="todo-wrapper">
+      <form action="#">
+        <input type="text" id="input-item" placeholder="Task...">
+        <button class="addBtn" onclick="addTodo()">Add</button>
+    </form>
+    
+    <ul id="todolist">
+      <todoItem />
+    </ul>
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
