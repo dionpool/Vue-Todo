07a4681753e256a67228c334994aeaa1d848d92f
Margrietj <margrietjellema32@gmail.com>
2023-01-11 15:07:29 +0100

    first item

--- src/components/todoItem.vue
+++ src/components/todoItem.vue
@@ -1,3 +1,27 @@
+<script>
+export default {
+    name: 'todoItem',
+    data () {
+      return {
+        todos: [
+            {
+                id: 1, 
+                title: "naam van todo",
+                done: false
+            },
+            {
+                id: 2, 
+                title: "leuke todo",
+                done: false
+            }
+        ]
+      }
+    }
+}
+</script>
+
 <template>
-    <li class="item"></li>
+    <li v-for="(item, index) in todos">
+        <li class="item">{{ item.title }}</li>
+    </li>
 </template>
