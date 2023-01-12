3eb002b73396214833cf6ba657df77caf6b6eaaa
Margrietj <margrietjellema32@gmail.com>
2023-01-12 12:14:00 +0100

    addtodo

---
+++ src/components/addToDo.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+    <form @submit="addToDo">
+      <input type="text" v-model="title" name="title">
+      <button type="submit">Add</button>
+    </form>
+  </div>
+</template>
